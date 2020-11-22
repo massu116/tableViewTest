728c1e080bc99725e4cf8703e62d877285ebd042
massu116 <kakeru1999116@yahoo.co.jp>
2020-11-23 04:55:19 +0900

    [add] tableView 表示が崩れてます

---
+++ tableViewTest/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  tableViewTest
+//
+//  Created by massu_fun on 2020/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var appealTable: UITableView!
+    
+    var ID = ["001", "002"]
+    var name = ["イカ", "ブリ"]
+    var Icon = ["ika", "yellowfish"]
+    var food = ["ikaring", "sashimi"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        appealTable.delegate = self
+        appealTable.dataSource = self
+    }
+    
+    //テーブルのセクション数
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    //テーブルの行数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    //テーブル1行ごとの設定
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.appealTable.dequeueReusableCell(withIdentifier: "appealCell", for: indexPath) as! appealTableViewCell
+        
+        cell.fishIcon.image = UIImage(named: self.Icon[indexPath.row])
+        cell.foodImage.image = UIImage(named: self.food[indexPath.row])
+        cell.fishID.text = self.ID[indexPath.row]
+        cell.fishName.text = self.name[indexPath.row]
+        
+        cell.fishIcon.layer.cornerRadius = 50
+        cell.foodImage.layer.cornerRadius = 10
+        return cell
+    }
+
+
+}
+
---
+++ tableViewTest/appealTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  appealTableViewCell.swift
+//  tableViewTest
+//
+//  Created by massu_fun on 2020/11/23.
+//
+
+import UIKit
+
+class appealTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var fishIcon: UIImageView!
+    @IBOutlet weak var fishID: UILabel!
+    @IBOutlet weak var fishName: UILabel!
+    @IBOutlet weak var foodImage: UIImageView!
+    
+}
